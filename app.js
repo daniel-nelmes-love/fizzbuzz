b7a6a2cb67b205f49b8b681952e1d15138f9b4cf
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-18 22:30:15 +1300

    duplicated if statements for i%5 and i%15

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
 	for (var i = 1; i <= 100; i++) {
-		if ((i%3)===0) {
+		if ((i%15)===0) {
+			$('body').append("<p>fizzbuzz</p>");
+		} else if ((i%5)===0) {
+			$('body').append("<p>buzz</p>");
+		} else if ((i%3)===0) {
 			$('body').append("<p>fizz</p>");
 		} else {
 			$('body').append("<p>" + i + "</p>")
