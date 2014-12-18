a70c43e8754d28ae0334581f647743fd45295a8f
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-18 22:40:00 +1300

    updated i%15 to be i%5 with another if inset for i%3 - same results produced but this time it is checking 3 and 5 separately rather than the product of these

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
 	for (var i = 1; i <= 100; i++) {
-		if ((i%15)===0) {
-			$('body').append("<p>fizzbuzz</p>");
-		} else if ((i%5)===0) {
-			$('body').append("<p>buzz</p>");
+		if ((i%5)===0) {
+			if ((i%3===0)) {
+				$('body').append("<p>fizzbuzz</p>");
+			} else {
+				$('body').append("<p>buzz</p>");
+			}
 		} else if ((i%3)===0) {
 			$('body').append("<p>fizz</p>");
 		} else {
