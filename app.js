4d5e7d53820a731e781a63ef29ffc73c49dcbe58
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2014-12-22 21:17:31 +1300

    moved loop to comment; created prompt

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
-	for (var i = 1; i <= 100; i++) {
-		if ((i%5)===0) {
-			if ((i%3===0)) {
-				$('body').append("<p>fizzbuzz</p>");
-			} else {
-				$('body').append("<p>buzz</p>");
-			}
-		} else if ((i%3)===0) {
-			$('body').append("<p>fizz</p>");
-		} else {
-			$('body').append("<p>" + i + "</p>")
-		}
-	}
+var inputValue = prompt("Please input a number from 1 to 100", "number");
+
+$('body').append(inputValue);
+
+
+//	for (var i = 1; i <= 100; i++) {
+//		if ((i%5)===0) {
+//			if ((i%3===0)) {
+//				$('body').append("<p>fizzbuzz</p>");
+//			} else {
+//				$('body').append("<p>buzz</p>");
+//			}
+//		} else if ((i%3)===0) {
+//			$('body').append("<p>fizz</p>");
+//		} else {
+//			$('body').append("<p>" + i + "</p>")
+//		}
+//	}
 })
